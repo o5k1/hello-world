daf26355485a194475031508422c1c7fa7b5b6bc
Andrea Tombolato <tombolato.andrea@gmail.com>
2016-04-22 13:32:01 +0200

    added fake test

---
+++ HelloWorld/src/test/java/HelloWorldTest.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.*;
+
+public class HelloWorldTest {
+
+    @org.junit.Test
+    public void testMain() throws Exception {
+        assertEquals("Messaggio errato","hello","world");
+    }
+}
