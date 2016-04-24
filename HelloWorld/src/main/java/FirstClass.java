523394550d7968ec60dc39d6ff0a3782f4225f84
Andrea Tombolato <tombolato.andrea@gmail.com>
2016-04-24 23:39:49 +0200

    added sumVar() in minus

--- HelloWorld/src/main/java/FirstClass.java
+++ HelloWorld/src/main/java/FirstClass.java
@@ -2,14 +2,32 @@
 public class FirstClass {
     private int a;
     private int b;
+    private String checkStrValue = null;
+
 
     public FirstClass(int a, int b) {
         this.a = a;
         this.b = b;
     }
 
-    public int sumVar(){ return a+b;}
+    public int sumVar() {
+        return a - b;
+    }
 
-    public int multiplyVar(){ return a*b;}
+    public int multiplyVar() {
+        return a * b;
+    }
 
+    /**
+     * Incorrect assignment in if condition.
+     */
+
+    public void incorrectAssignmentInIfCondition() {
+        boolean value = false;
+        if (value = true) {
+            //do Something
+        } else {
+            //else Do Something
+        }
+    }
 }
--- HelloWorld/src/main/java/HelloWorld.java
+++ HelloWorld/src/main/java/HelloWorld.java
@@ -2,5 +2,9 @@ public class HelloWorld {
     public static void main(String[] args) {
         String message = "Ciao bbbbellli!";
         System.out.println(message);
+
+        FirstClass f = new FirstClass(1,2);
+        f.incorrectAssignmentInIfCondition();
+
     }
 }
