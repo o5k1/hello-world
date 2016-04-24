8c2dd3d177027e9242d0e762fdccf00a97206bef
Andrea Tombolato <tombolato.andrea@gmail.com>
2016-04-25 00:31:45 +0200

    added

---
+++ HelloWorld/src/main/java/SecondClass.java
@@ -0,0 +1,13 @@
+/**
+ * Created by Andrea on 25/04/2016.
+ */
+public class SecondClass {
+
+    public static String fooA(){
+        return "fooB";
+    }
+
+    public static String fooB(){
+        return "fooB";
+    }
+}
